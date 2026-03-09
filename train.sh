2a609ff1b74843bb2d53fa96b02f5c8a3b314d21
Valentin Bosch <val.bosch2001@gmail.com>
2026-03-09 14:43:19 +0100

    up

--- train.sh
+++ train.sh
@@ -5,7 +5,9 @@
 #$ -N mil_train
 cd /beegfs/data/work/c-2iia/vb710264/mil_lab
 module load python/3.11/anaconda/2024.02
-source /beegfs/data/work/c-2iia/vb710264/trident/trident_venv/bin/activate
+bash
+source ~/.bashrc
+conda activate trident_env311
 
 cd /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil
 python /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil/feature_extraction.py
