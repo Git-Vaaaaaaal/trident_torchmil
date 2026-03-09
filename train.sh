b4a659b4d7f0381d32ff52368cc715a36d055174
Valentin Bosch <val.bosch2001@gmail.com>
2026-03-09 14:43:05 +0100

    up

--- train.sh
+++ train.sh
@@ -4,10 +4,9 @@
 #$ -o result.out
 #$ -N mil_train
 cd /beegfs/data/work/c-2iia/vb710264/mil_lab
-module load python
+module load python/3.11/anaconda/2024.02
 source /beegfs/data/work/c-2iia/vb710264/trident/trident_venv/bin/activate
-export PYTHONPATH=/work/c-2iia/vb710264/trident/trident_venv/lib/python3.9/site-packages:$PYTHONPATH
-export MPLCONFIGDIR=/work/c-2iia/vb710264/.cache/matplotlib
+
 cd /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil
 python /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil/feature_extraction.py
 python /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil/torchmil_process.py
