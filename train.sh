eb058b3923212b6383bd0281a5de249d8e05eb09
Valentin Bosch <val.bosch2001@gmail.com>
2026-03-09 14:33:54 +0100

    up

--- train.sh
+++ train.sh
@@ -5,8 +5,9 @@
 #$ -N mil_train
 cd /beegfs/data/work/c-2iia/vb710264/mil_lab
 module load python
-source /beegfs/data/work/c-2iia/vb710264/mil_lab/milab_venv/bin/activate
-export PYTHONPATH=/work/c-2iia/vb710264/mil_lab/milab_venv/lib/python3.9/site-packages:$PYTHONPATH
+source /beegfs/data/work/c-2iia/vb710264/trident/trident_venv/bin/activate
+export PYTHONPATH=/work/c-2iia/vb710264/trident/trident_venv/lib/python3.9/site-packages:$PYTHONPATH
 export MPLCONFIGDIR=/work/c-2iia/vb710264/.cache/matplotlib
-cd /beegfs/data/work/c-2iia/vb710264/mil_lab/MIL-Lab
-python /beegfs/data/work/c-2iia/vb710264/mil_lab/MIL-Lab/src/train.py --task coords --wsi_dir ./wsis --job_dir ./trident_processed --mag 20 --patch_size 256 --overlap 0
+cd /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil
+python /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil/feature_extraction.py
+python /beegfs/data/work/c-2iia/vb710264/trident/trident_torchmil/torchmil_process.py
