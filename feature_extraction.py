c47447d64790e403de0264445e35abcb75d2697d
Valentin Bosch <val.bosch2001@gmail.com>
2026-03-13 10:11:56 +0100

    up

--- feature_extraction.py
+++ feature_extraction.py
@@ -13,15 +13,15 @@ from trident import OpenSlideWSI
 from trident.segmentation_models import segmentation_model_factory
 from model_mode import options as options
 
-OPENSLIDE_PATH = r'openslide_win64\\openslide_win64'
-
+""" OPENSLIDE_PATH = r'openslide_win64\\openslide_win64'
+ """
 import os
-if hasattr(os, 'add_dll_directory'):
+""" if hasattr(os, 'add_dll_directory'):
     # Windows
     with os.add_dll_directory(OPENSLIDE_PATH):
         import openslide
-else:
-    import openslide
+else: """
+import openslide
 
 #variables
 list_markers = ["BCL2", "BCL6", "HE", "MUM1", "MYC"] # "BCL2", "BCL6", "HE", "MUM1", "MYC"
